3c9ee936586fc76e3a570647b1f8c3bd19730197
kvaled@gmail.com <kvaled@gmail.com>
2018-05-10 19:19:28 -0500

    update charts

--- R/aqi_waffle.R
+++ R/aqi_waffle.R
@@ -1,16 +1,13 @@
-#devtools::install_github("hrbrmstr/waffle")
-library(waffle)
+library(waffle) #devtools::install_github("hrbrmstr/waffle")
+
+pm_sums <- group_by(pm_data, site_catid, aqi_color) %>% summarize(count = n()) %>% arrange(desc(aqi_color)) %>% ungroup() %>% mutate(aqi_color = forcats::fct_recode(aqi_color, Good = "#53BF33", Moderate = "#FFEE00", `Unhealthy` = "#DB6B1A"))
 
 iron(
+  waffle(filter(pm_sums, site_catid == "27-017-7417") %>% select(aqi_color, count), 
+         rows = 7,  size = 0.5, colors = c("#65B345", "#f2d01a"),
+         title  = "Fond du Lac AQI :: 2015"),
   
-waffle(data_2014, rows = 7,  size = 0.5, 
-       colors = aqi_colors,
-       xlab   = "1 square = 1 day", title  = "AQI days for Fond du Lac\n2014",
-       flip   = TRUE)
-
-waffle(data_2015, rows = 7,  size = 0.5, 
-       colors = aqi_colors,
-       xlab   = "1 square = 1 day", title  = "AQI days for Fond du Lac\n2015",
-       flip   = TRUE)
-
+  waffle(filter(pm_sums, site_catid == "27-137-7549") %>% select(aqi_color, count), 
+         rows = 7,  size = 0.5, colors = rev(c("#F26522", "#F2D01A", "#65B345")),
+         xlab   = "1 square = 1 day", title  = "Duluth AQI :: 2015")
 )
